55da8590a682d8f41a714dca3cf4bcf00c37d98a
Domingo Luna <mingo_pic_sunday@hotmail.com>
2018-05-24 16:42:36 -0500

    Se agregan campos de config de mermas para la tabla tb_recepcionCuero

--- esmarProdBD.sql
+++ esmarProdBD.sql
@@ -76,6 +76,10 @@ create table tb_recepcionCuero (
 	, mermaHumedad float
 	, mermaCachete float
 	, mermaTarimas float
+	, idMerSal int
+	, idMerHum int
+	, idMerCac int
+	, idMerTar int
 	, fechaEntrada date
 );
 go

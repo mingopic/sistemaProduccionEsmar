96b48b6a343e2b2b84e9ab65a4ac95e074ff56a8
Mingo <Mingo@Mingo-Lap>
2018-08-24 01:02:22 -0500

    - Se añade SP para insertar en tb_cueroPesado - Se añade update en la configuración de tb_tipoRecorte para reemplazar caracteres erroneos por la letra ñ

--- Base de datos/4. Configuraciones/09. tb_tipoRecorte.sql
+++ Base de datos/4. Configuraciones/09. tb_tipoRecorte.sql
@@ -5,3 +5,8 @@ insert into
   tb_tipoRecorte
 values
   ('Entero') ,('Delantero Sillero'), ('Crupon Sillero'), ('Lados'), ('Centro Castaño'), ('Centro Quebracho'), ('Delantero Suela');
+
+update 
+  tb_tipoRecorte 
+set 
+  descripcion = replace(descripcion, 'Ã±', 'ñ')
